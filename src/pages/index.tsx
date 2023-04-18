7cdf6a91e4bf5b32914d588ce01dfc272560c922
SHUKUR <ziyodullayevshukur1998@gmail.com>
2023-04-19 02:55:12 +0500

    adding getTableLis in tablesSlice and testing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,15 +3,22 @@ import SEO from '@/layout/seo/seo'
 import Layout from '@/layout/layout'
 import { AppDispatch, RootState } from "@/store/store";
 import { useSelector, useDispatch } from 'react-redux';
-import { getAllTableList } from '@/slices/tablesSlice';
+import { getAllTableList, getTableList } from '@/slices/tablesSlice';
 
 export default function Home() {
   const dispatch = useDispatch<AppDispatch>();
-  const {allTableList} = useSelector((state: RootState) => state.tables)
+  const {allTableList, tableList} = useSelector((state: RootState) => state.tables)
   console.log("allTableList:", allTableList);
+  console.log("tableList:", tableList);
   const GetAllTableList = () => {
     dispatch(
       getAllTableList({
+        start: "2023-04-18",
+        end: "2023-04-18"
+      })
+    );
+    dispatch(
+      getTableList({
         start: "2023-04-18",
         end: "2023-04-18"
       })
--- src/slices/tablesSlice.ts
+++ src/slices/tablesSlice.ts
@@ -4,10 +4,7 @@ import axios from 'axios';
 const api_url = process.env.NEXT_PUBLIC_API_URL as string;
 
 interface TableState {
-  tableList: {
-    count: number;
-    results: string[];
-  },
+  tableList: GetTableListResponseData | null,
   allTableList: GetAllTableListResponseData | null;
   faculties: string[];
   groups: string[];
@@ -49,10 +46,73 @@ interface GetAllTableListFormValues {
   end: Date | string;
 }
 
+interface GetTableListResponseData {
+  count: number | null;
+  next: string | null;
+  previous: string | null;
+  results: [
+    {
+      id: number | null;
+      owner: string | null;
+      group: string[] | null;
+      percent: number | null;
+      room: string | null;
+      deputy_dean: string[] | null;
+      faculty: string | null;
+      course: string | null;
+      para: number | null;
+      subject: string | null;
+      teacher: string | null;
+      department: string | null;
+      sum_student: number | null;
+      absent_student: number | null;
+      description: string | null;
+      description_2: string | null;
+      sport: number | null;
+      exchange: number | null;
+      created_at: string | null;
+      updated_at: string | null;
+      dean: string | null;
+      tutor: string[] | null;
+    },
+
+  ];
+} 
+
+interface GetTableListFormValues {
+  start: Date | string;
+  end: Date | string;
+}
+
 const initialState: TableState = {
   tableList: {
     count: 0,
-    results: [],
+    next: null,
+    previous: null,
+    results: [{
+      id: null,
+      owner: null,
+      group: null,
+      percent: null,
+      room: null,
+      deputy_dean: null,
+      faculty: null,
+      course: null,
+      para: null,
+      subject: null,
+      teacher: null,
+      department: null,
+      sum_student: null,
+      absent_student: null,
+      description: null,
+      description_2: null,
+      sport: null,
+      exchange: null,
+      created_at: null,
+      updated_at: null,
+      dean: null,
+      tutor: null,
+    }],
   },
   allTableList: null,
   faculties: [],
@@ -64,6 +124,7 @@ const initialState: TableState = {
   tablesError: null,
   tablesErrorStatus: false
 };
+
 
 export const getAllTableList = createAsyncThunk<GetAllTableListResponseData, GetAllTableListFormValues>(
   'tables/getAllTableList',
@@ -77,10 +138,25 @@ export const getAllTableList = createAsyncThunk<GetAllTableListResponseData, Get
   }
 );
 
+export const getTableList = createAsyncThunk<GetTableListResponseData, GetTableListFormValues>(
+  'tables/getTableList',
+  async (param) => {
+    let datetest = JSON.stringify({
+      start: param.start,
+      finish: param.end,
+    });
+    const response = await axios.get<GetTableListResponseData>(api_url + "/api/v1/attendance-list/", {params: {limit: 50, date: datetest}});
+    return response.data;
+  }
+);
+
 const tablesSlice = createSlice({
   name: 'tables',
   initialState,
   reducers: {
+    setErrorAction: (state, action) => {
+      state.tablesErrorStatus = action.payload;
+    },
   },
   extraReducers: (builder) => {
     builder
@@ -98,8 +174,22 @@ const tablesSlice = createSlice({
         state.tablesStatus = 'failed';
         state.tablesError = action.error.message ?? 'Login failed';
         state.tablesErrorStatus = true;
+      })
+      .addCase(getTableList.pending, (state) => {
+        state.tablesStatus = 'loading';
+        state.tablesErrorStatus = false;
+      })
+      .addCase(getTableList.fulfilled, (state, action) => {
+        state.tablesStatus = 'idle';
+        state.tableList = action.payload;
+        state.tablesError = null;
+        state.tablesErrorStatus = false;
+      })
+      .addCase(getTableList.rejected, (state, action) => {
+        state.tablesStatus = 'failed';
+        state.tablesError = action.error.message ?? 'Login failed';
+        state.tablesErrorStatus = true;
       });
   },
 });
-
 export default tablesSlice.reducer
